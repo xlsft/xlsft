0b853e9b32efa07164c2a839713431b4f7d890cd
XL Software <dev@xlsoftware.ru>
2025-07-17 00:45:49 +0300

    refactor: cleanup

--- components/blocks/Projects.vue
+++ components/blocks/Projects.vue
@@ -1,27 +1,27 @@
 <script setup lang="ts">
+    import { useDragger } from '@xlsft/nuxt';
     import ProjectCard from '../global/ProjectCard.vue';
-    import { useDragger } from "@xlsft/nuxt"
 
     const dragger = useDragger({ drag: true, scroll: true, direction: 'x' })
     const container = ref<HTMLElement>()
     onMounted(() => { dragger.init(container.value) })
     onUnmounted(() => dragger.destroy())
     const { t } = useI18n()
-    const items = computed<{ id: string, name: string, description: string, link: string, tags: string[] }[]>(() => [
-        { id: 'ained_widget', name: t('project.ained_widget.name'), description: t('project.ained_widget.description'), link: '/demo/ained_widget', tags: ['ained'] },
-        { id: 'ained_developer', name: t('project.ained_developer.name'), description: t('project.ained_developer.description'), link: 'https://developer.ained.ru/', tags: ['ained'] },
-        { id: 'briskly_business', name: t('project.briskly_business.name'), description: t('project.briskly_business.description'), link: 'https://briskly.business', tags: ['briskly'] },
-        { id: 'ained_b24', name: t('project.ained_b24.name'), description: t('project.ained_b24.description'), link: 'https://www.bitrix24.ru/apps/app/ained.ained_chess', tags: ['ained'] },
-        { id: 'work_russia', name: t('project.work_russia.name'), description: t('project.work_russia.description'), link: 'https://guide.cznnew.ru/', tags: ['xlsft'] },
-        { id: 'xlsft_nuxt', name: t('project.xlsft_nuxt.name'), description: t('project.xlsft_nuxt.description'), link: 'https://jsr.io/@xlsft/nuxt', tags: ['xlsft', 'oss', 'lib'] },
-        { id: 'ained_widget_bot', name: t('project.ained_widget_bot.name'), description: t('project.ained_widget_bot.description'), link: 'https://t.me/ained_widget_bot', tags: ['ained'] },
-        { id: 'smartshell_sdk', name: t('project.smartshell_sdk.name'), description: t('project.smartshell_sdk.description'), link: 'https://smartshell.xlsft.ru', tags: ['lib', 'oss'] },
-        { id: 'xlkbd', name: t('project.xlkbd.name'), description: t('project.xlkbd.description'), link: 'https://github.com/xlsft/xlkbd', tags: ['oss'] },
-        { id: 'htsx', name: t('project.htsx.name'), description: t('project.htsx.description'), link: 'https://github.com/xlsft/htsx', tags: ['oss', 'lib'] },
-        { id: 'xlui', name: t('project.xlui.name'), description: t('project.xlui.description'), link: 'https://github.com/xlsft/ui', tags: ['oss'] },
-        { id: 'gizmo_parser', name: t('project.gizmo_parser.name'), description: t('project.gizmo_parser.description'), link: 'https://github.com/xlsft/gizmo-parser', tags: ['oss'] },
-        { id: 'yasearch', name: t('project.yasearch.name'), description: t('project.yasearch.description'), link: 'https://addons.mozilla.org/ru/firefox/addon/yasearch/', tags: ['oss','popular'] },
-        { id: 'cyber_bot', name: t('project.cyber_bot.name'), description: t('project.cyber_bot.description'), link: 'https://github.com/xlsft/cyber-bot', tags: ['oss','tg'] },
+    const items = computed<{ id: string, link: string, tags: string[] }[]>(() => [
+        { id: 'ained_widget', link: '/demo/ained_widget', tags: ['ained'] },
+        { id: 'ained_developer', link: 'https://developer.ained.ru/', tags: ['ained'] },
+        { id: 'briskly_business', link: 'https://briskly.business', tags: ['briskly'] },
+        { id: 'ained_b24', link: 'https://www.bitrix24.ru/apps/app/ained.ained_chess', tags: ['ained'] },
+        { id: 'work_russia', link: 'https://guide.cznnew.ru/', tags: ['xlsft'] },
+        { id: 'xlsft_nuxt', link: 'https://jsr.io/@xlsft/nuxt', tags: ['xlsft', 'oss', 'lib'] },
+        { id: 'ained_widget_bot', link: 'https://t.me/ained_widget_bot', tags: ['ained'] },
+        { id: 'smartshell_sdk', link: 'https://smartshell.xlsft.ru', tags: ['lib', 'oss'] },
+        { id: 'xlkbd', link: 'https://github.com/xlsft/xlkbd', tags: ['oss'] },
+        { id: 'htsx', link: 'https://github.com/xlsft/htsx', tags: ['oss', 'lib'] },
+        { id: 'xlui', link: 'https://github.com/xlsft/ui', tags: ['oss'] },
+        { id: 'gizmo_parser', link: 'https://github.com/xlsft/gizmo-parser', tags: ['oss'] },
+        { id: 'yasearch', link: 'https://addons.mozilla.org/ru/firefox/addon/yasearch/', tags: ['oss','popular'] },
+        { id: 'cyber_bot', link: 'https://github.com/xlsft/cyber-bot', tags: ['oss','tg'] },
     ])
 
     const counts = computed(() => {
--- components/global/ProjectCard.vue
+++ components/global/ProjectCard.vue
@@ -6,7 +6,7 @@
         window.open(props.link, '_blank')
     })
     
-    const props = defineProps<{ id: string, name: string, description: string, link: string, tags: string[] }>()
+    const props = defineProps<{ id: string, link: string, tags: string[] }>()
     const { t } = useI18n()
     const router = useRouter()
 </script>
@@ -16,7 +16,7 @@
     <div class="group flex flex-col overflow-hidden border cursor-nw-resize! *:cursor-nw-resize! hover:border-blue-500! group w-[300px] min-w-[300px] h-[calc(400px-96px)] my-[48px] mr-[24px] first:ml-[48px] last:mr-[48px]" ref="container">
         <img :src="`/projects/${props.id}.webp`" class="w-full grayscale-100 group-hover:grayscale-0">
         <div class="flex px-[24px] pb-[24px] flex-col justify-between grow">
-            <span class="text-xs! opacity-50">{{ props.description }}</span>
+            <span class="text-xs! opacity-50">{{ t(`project.${props.id}.description`) }}</span>
             <div class="flex flex-wrap gap-[4px]">
                 <div class="text-xs! px-[8px] py-[2px] border" v-for="tag in props.tags">{{ tag }}</div>
             </div>
