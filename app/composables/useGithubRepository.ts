e6118afbd236c961ee1b38ea3a5572906a8ff1c2
xlsft <dev@xlsft.ru>
2026-03-04 16:50:21 +0300

    refactor: replace ky with useFetch for GitHub repository data fetching

--- app/composables/useGithubRepository.ts
+++ app/composables/useGithubRepository.ts
@@ -1,38 +1,15 @@
-import ky, { HTTPError, type Options } from 'ky'
-
 export const useGithubRepository = async <ResponseData = unknown, Response = ResponseData>(
-    endpoint: string,
-    options?: Options
+    endpoint: string
 ): Promise<Response> => {
-
-    const headers = {
-        'Accept': 'application/json',
-        ...options?.headers ? options.headers : {},
-    }
-
     let response: Response
     try {
-        response = await ky(endpoint, {
-            ...options,
-            prefixUrl: `https://api.github.com/repos`,
-            timeout: false,
-            retry: 0,
-            hooks: {
-                beforeRequest: [(request) => Object.entries(headers).forEach(([key, value]) => request.headers.set(key, value as string))],
-                beforeError: [
-                    async (error: HTTPError) => {
-                        response = (await error.response.json()) as Response;
-                        error.name = 'ResponseError';
-                        return error;
-                    },
-                ],
-                ...options?.hooks ? options.hooks : {},
-            },
-        }).json<Response>();
+        response = (await useFetch<Response>(endpoint, {
+            baseURL: `/api/repo`,
+            responseType: 'json',
+        })).data.value as Response
         return response;
     } catch (error) {
         console.error(error);
         return response!;
     }
-
 }
