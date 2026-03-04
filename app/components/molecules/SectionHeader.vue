2e2b6f04b37465ec081a5c5c63b605399cce6314
xlsft <dev@xlsft.ru>
2026-03-04 16:52:46 +0300

    feat: add ContactMe component with SectionHeader integration and toggle functionality

--- app/components/molecules/SectionHeader.vue
+++ app/components/molecules/SectionHeader.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-    const props = defineProps<{ pattern: string, id?: string }>()
+    const props = defineProps<{ pattern: string, id?: string, noContact?: boolean }>()
 </script>
 
 <template>
-    <AtomsPattern :name="props.pattern" :id class="w-full py-12 outline group/header hover:outline-blue-500! transition-colors z-2 flex items-center justify-center" :class="$slots.default ? 'min-h-32' : 'min-h-18'">
-        <h2 class="text-center bg-default py-3 px-6 border group-hover/header:border-blue-500! text-2xl! font-bold z-3 pointer-events-none" v-if="$slots.default">
+    <AtomsPattern :name="props.pattern" :id class="w-full py-12 outline group/header hover:outline-blue-500! transition-colors z-2 flex items-center justify-between! px-8 max-lg:flex-col gap-4 max-lg:py-4 max-lg:px-0" :class="$slots.default ? 'min-h-32' : 'min-h-18'">
+        <h2 class="text-left bg-default py-3 px-6 border group-hover/header:border-blue-500! text-2xl! font-bold z-3 pointer-events-none" v-if="$slots.default">
             <slot/>
         </h2>
+        <OrganismsContactMe class="z-3" :id v-if="$slots.default && !props.noContact"/>
         <slot name="outer"/>
     </AtomsPattern>
 </template>
---
+++ app/components/organisms/ContactMe.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import type { HTMLAttributes } from 'vue';
+    import SectionHeader from '../molecules/SectionHeader.vue';
+
+    const { t } = useI18n()
+    const props = defineProps<{ id?: string, class?: HTMLAttributes['class'] }>()
+
+    const open = ref<boolean>(false)
+</script>
+
+<template>
+    <NuxtButton size="xl" :class @click="open = !open">{{ t('labels.contact_me') }}</NuxtButton>
+    <NuxtModal v-model:open="open">
+        <template #content>
+            <SectionHeader no-contact pattern="topography"/>
+        </template>
+    </NuxtModal>
+</template>
