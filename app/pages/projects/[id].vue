8e6077f24b4df97cba994a316e23f1407e852b00
xlsft <dev@xlsft.ru>
2026-03-05 10:25:58 +0300

    feat: enhance project page with related projects and SEO improvements

--- app/pages/projects/[id].vue
+++ app/pages/projects/[id].vue
@@ -2,10 +2,16 @@
     import Skeleton from '~/assets/svg/skeleton.svg'
     import useMarkdownRemoval from 'remove-markdown'
 
-    const { t } = useI18n()
+    const { t, locale } = useI18n()
     const route = useRoute()
+    const router = useRouter()
+    const theme = useColorMode()
 
+    const params = ref({ id: route.params.id?.toString() }); watch(() => route.params.id, () => params.value.id = route.params.id?.toString())
     const { data } = await useSanityDynamicQuery<any>(groq`*[_type == "project" && id == $id] {
+        "seo": *[_type == "summary"][0] {
+            "title": title[$locale], 
+        },
         id, links, tags, repo,
         "name": name[$locale],
         "description": description[$locale],
@@ -19,7 +25,13 @@
         } | order(priority desc),
         "status": status[$locale],
         "copyright": copyright[$locale],
-    }[0]`, ref({ id: route.params.id?.toString() }))
+        "related": related[]->{
+            id, tags, repo, priority,
+            "name": name[$locale],
+            "thumbnail": thumbnail[$theme].asset->url,
+            "description": description[$locale]
+        }
+    }[0]`, params);
 
     if (!data.value) throw createError({ status: 404, message: `"${route.params.id?.toString()}" Project not found` })
 
@@ -27,14 +39,21 @@
 
     useSeoMeta({
         title: data.value?.name,
-        description: useMarkdownRemoval(data.value?.description)
+        ogTitle: `${data.value?.seo.title} — ${data.value?.name}`,
+        description: useMarkdownRemoval(data.value?.description),
+        ogDescription: useMarkdownRemoval(data.value?.description)
+    })
+
+    defineOgImage('AtomsOgProject.takumi', {
+        theme: theme.value as 'dark' | 'light',
+        project: data.value,
     })
 
 </script>
 
 <template>
     <NuxtCarousel
-        v-if="data?.images?.length"
+        v-if="data?.images?.length && data?.images?.length > 1"
         :items="data?.images" v-slot="{ item: src, index }"
         loop autoplay arrows
         :prev="{ variant: 'outline' }" :next="{ variant: 'outline' }"        
@@ -47,29 +66,51 @@
         }"
     >
         <AtomsPreview class="relative" :style="{ background: data?.bg }">
-            <NuxtImg :src="(src as string)", :alt="data?.name + ` image ${index}`" class="transition-all select-none h-100 w-full object-contain object-center!" :placeholder="Skeleton" placeholder-class="animate-pulse blur-lg scale-150"/>
+            <NuxtImg 
+                :src="(src as string)"
+                :alt="data?.name + ` image ${index}`" 
+                class="transition-all select-none h-100 w-full object-contain object-center!" 
+                :placeholder="Skeleton" 
+                placeholder-class="animate-pulse blur-lg scale-150"
+            />
         </AtomsPreview>
     </NuxtCarousel>
+    <AtomsPreview v-else class="relative" :style="{ background: data?.bg }">
+        <NuxtImg 
+            :src="(data?.images?.[0] as string)"
+            :alt="data?.name + ` image ${0}`" 
+            class="transition-all select-none h-100 w-full object-contain object-center!" 
+            :placeholder="Skeleton" 
+            placeholder-class="animate-pulse blur-lg scale-150"
+        />
+    </AtomsPreview>
+
+
     <MoleculesSectionHeader pattern="graph-paper">
         <div>{{ t('sections.about_project') }}</div>
     </MoleculesSectionHeader>
     <section class="p-8 max-lg:p-4 flex max-lg:flex-col-reverse gap-8 max-lg:gap-4">
+
         <div class="flex flex-col gap-4 grow">
             <NuxtImg :src="data?.thumbnail" :alt="data?.description" v-if="data?.thumbnail" class="w-full border"/>
             <div class="flex items-center gap-4">
                 <h1>{{ data?.name }}</h1>
-                <div class="flex items-center gap-1 *:text-yellow-500" v-if="repo?.stargazers_count">
-                    <span>{{ repo?.stargazers_count }}</span>
+                <div class="flex items-center gap-1 *:text-yellow-500" v-if="repo?.stars">
+                    <span>{{ repo?.stars }}</span>
                     <NuxtIcon name="mingcute:star-fill"/>
                 </div>
             </div>
             <MDC :value="data?.about" class="md *:mt-0! *:last:mt-0!"/>
             <p class="text-sm text-default/50" v-if="data?.copyright">{{ data?.copyright }}</p>
         </div>
+
         <div class="flex flex-col items-end max-lg:items-start gap-2 min-w-[30%]!">
+
             <NuxtBadge v-if="data?.status" :class="data?.images?.length ? `absolute top-8 right-8 max-lg:top-4 max-lg:right-4 z-2` : ''" variant="solid">{{ data?.status }}</NuxtBadge>
+
             <h3 class="text-lg!" v-if="data?.description">{{ t('labels.about') }}:</h3>
             <MDC v-if="data?.description" :value="data?.description" class="md *:m-0! *:text-sm! *:leading-4! text-default/50 *:text-right max-lg:*:text-left!"/>
+
             <h3 class="text-lg!" v-if="repo?.topics || data?.tags">{{ t('labels.tags') }}:</h3>
             <div class="flex flex-wrap gap-1 justify-end items-center" v-if="data?.tags">  
                 <NuxtBadge v-for="tag in data?.tags" :key="tag" variant="outline" color="neutral">{{ tag }}</NuxtBadge>
@@ -77,6 +118,7 @@
             <div class="flex flex-wrap gap-1 justify-end items-center" v-if="repo?.topics">  
                 <NuxtBadge v-for="topic in repo?.topics" :key="topic" variant="outline" color="neutral">{{ topic }}</NuxtBadge>
             </div>
+
             <h3 class="text-lg!" v-if="data?.stack">{{ t('labels.stack') }}:</h3>
             <div id="stack" class="flex flex-wrap gap-1 justify-end items-center" v-if="data?.stack">  
                 <NuxtBadge 
@@ -89,10 +131,47 @@
                     {{ item.name }}
                 </NuxtBadge>
             </div>
+            
             <h3 class="text-lg!" v-if="data?.links">{{ t('labels.links') }}:</h3>
             <NuxtLink v-for="link in data?.links" :key="link" :to="link" class="flex items-center gap-1" v-if="data?.links">
                 {{ link }}
             </NuxtLink>
         </div>
     </section>
+    <template v-if="data.related?.length">
+        <MoleculesSectionHeader pattern="graph-paper" id="projects">
+            <div>{{ t('sections.related_projects') }}</div>
+        </MoleculesSectionHeader>
+        <section class="flex flex-col relative">
+            <NuxtBlogPosts class="p-8 max-lg:p-4" :ui="{ base: 'gap-4! p-8!' }">
+                <NuxtBlogPost
+                    v-for="project in (data.related || []).sort((a: any, b: any) => b.priority - a.priority)"
+                    :key="project.name"
+                    variant="outline"
+                    :image="project.thumbnail"
+                    @click="() => { router.push(`/${locale}/projects/${project.id}`) }"
+                    :ui="{
+                        header: 'border-b group-hover/blog-post:border-primary!', 
+                        image: 'grayscale group-hover/blog-post:grayscale-0 group-hover/blog-post:scale-105! transition-all',
+                        root: 'hover:ring-primary! *:cursor-nw-resize! cursor-nw-resize!',
+                        body: 'p-3! flex flex-col gap-1',
+                        description: 'm-0! flex flex-col gap-4 grow',
+                        title: 'text-sm!'
+                    }"
+                >
+                    <template #title>
+                        <div class="flex items-center justify-between w-full gap-2">
+                            <span>{{ project.name }}</span>
+                        </div>
+                    </template>
+                    <template #description>
+                        <MDC :value="project.description" class="md *:m-0! *:text-xs! *:leading-4! grow"/>
+                        <div class="flex flex-wrap gap-1">  
+                            <NuxtBadge v-for="tag in project.tags" :key="tag" variant="outline" color="neutral" size="sm">{{ tag }}</NuxtBadge>
+                        </div>
+                    </template>
+                </NuxtBlogPost>
+            </NuxtBlogPosts>
+        </section>
+    </template>
 </template>
