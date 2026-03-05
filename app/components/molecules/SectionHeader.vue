faaf6d03e931803ee63fc51ed5c29a3cd2b32f7a
xlsft <dev@xlsft.ru>
2026-03-05 10:20:38 +0300

    refactor: improve layout and class binding in SectionHeader component

--- app/components/molecules/SectionHeader.vue
+++ app/components/molecules/SectionHeader.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
-    <AtomsPattern :name="props.pattern" :id class="w-full py-12 outline group/header hover:outline-blue-500! transition-colors z-2 flex items-center justify-between! px-8 max-lg:flex-col gap-4 max-lg:py-4 max-lg:px-0" :class="$slots.default ? 'min-h-32' : 'min-h-18'">
+    <AtomsPattern 
+        :name="props.pattern" 
+        :id 
+        class="w-full py-12 outline group/header hover:outline-blue-500! transition-colors z-2 flex items-center px-8 max-lg:flex-col gap-4 max-lg:py-4 max-lg:px-0" 
+        :class="[
+            $slots.default ? 'min-h-32' : 'min-h-18',
+            props.noContact ? 'justify-center' : 'justify-between!'
+        ]"
+    >
         <h2 class="text-left bg-default py-3 px-6 border group-hover/header:border-blue-500! text-2xl! font-bold z-3 pointer-events-none" v-if="$slots.default">
             <slot/>
         </h2>
