e4e97f2edc4ac63acb8a16e724c4678caf96e088
xlsft <dev@xlsft.ru>
2026-03-05 20:07:52 +0300

    feat: web-vitals

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+    import * as wv from 'web-vitals/attribution'
 
+    const vitals = ref<Record<string, any>>({})
+
+
+    onMounted(() => {
+        wv.onCLS(console.info)
+        wv.onFCP(console.info)
+        wv.onINP(console.info)
+        wv.onLCP(console.info)
+        wv.onTTFB(console.info)
+    })
 </script>
 
 <template>
