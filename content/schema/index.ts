9f7f8c8aa2f17b2d21fa10af5c744f088a81d3fa
xlsft <dev@xlsft.ru>
2026-03-05 10:33:04 +0300

    feat: index legal type

--- content/schema/index.ts
+++ content/schema/index.ts
@@ -4,9 +4,10 @@ import link from './link.type'
 import experience from './experience.type'
 import project from './project.type'
 import education from './education.type'
+import legal from './legal.type'
 
 export const schemaTypes = [
-    skill, summary, link, experience, project, education
+    skill, summary, link, experience, project, education, legal
 ]
 
 export default schemaTypes
