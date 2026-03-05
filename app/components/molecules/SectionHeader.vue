46c4c8ccb57a731f0dfd09da7a620f78c83917d8
xlsft <dev@xlsft.ru>
2026-03-05 20:05:39 +0300

    feat: print css

--- app/components/molecules/SectionHeader.vue
+++ app/components/molecules/SectionHeader.vue
@@ -6,15 +6,16 @@
     <AtomsPattern 
         :name="props.pattern" 
         :id 
-        class="w-full py-12 outline group/header hover:outline-blue-500! transition-colors z-2 flex items-center px-8 max-lg:flex-col gap-4 max-lg:py-4 max-lg:px-0" 
+        style="break-before: page;"
+        class="w-full py-12 outline group/header hover:outline-blue-500! print:border print:outline-0 transition-colors z-2 flex items-center px-8 max-lg:flex-col gap-4 max-lg:py-4 max-lg:px-0 print:p-0! print:items-center! print:justify-center!" 
         :class="[
             $slots.default ? 'min-h-32' : 'min-h-18',
             props.noContact ? 'justify-center' : 'justify-between!'
         ]"
     >
-        <h2 class="text-left bg-default py-3 px-6 border group-hover/header:border-blue-500! text-2xl! font-bold z-3 pointer-events-none" v-if="$slots.default">
+        <h2 class="max-lg:text-center bg-default py-3 px-6 border group-hover/header:border-blue-500! text-2xl! font-bold z-3 pointer-events-none" v-if="$slots.default">
             <slot/>
         </h2>
-        <OrganismsContactMe class="z-3" :id v-if="$slots.default && !props.noContact"/>
+        <OrganismsContactButton class="z-3" :id v-if="$slots.default && !props.noContact"/>
     </AtomsPattern>
 </template>
