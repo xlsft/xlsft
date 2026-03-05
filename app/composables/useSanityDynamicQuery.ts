07ff9f0d878ec45b133e3b5a08a9adca0eab7bd6
xlsft <dev@xlsft.ru>
2026-03-05 10:20:46 +0300

    fix: include route fullPath in useAsyncData query construction

--- app/composables/useSanityDynamicQuery.ts
+++ app/composables/useSanityDynamicQuery.ts
@@ -6,17 +6,25 @@ export const useSanityDynamicQuery = async <T = any>(
     options: AsyncDataOptions<T> = {}
 ) => {
     const theme = useColorMode()
+    const route = useRoute()
     const { locale } = useI18n()
     const sanity = useSanity()
 
     const constructParams = () => ({ ...params?.value || {}, locale: locale.value || 'ru', theme: theme.value || 'dark' })
 
-    const data = useAsyncData<T>(query, () => {
+    const data = useAsyncData<T>(query + route.fullPath, () => {
         const params = constructParams()
         return sanity.fetch(query, params)
     }, options)
+    
+    watch(
+        () => params?.value,
+        console.info,
+        { deep: true }
+    )
 
     watch(() => [locale.value, theme.value, params?.value], () => {
+        console.log(params?.value)
         const keys = Object.keys(constructParams())
         if (keys.some(key => query.includes(`$${key}`))) data.refresh()
     }, { deep: true })
