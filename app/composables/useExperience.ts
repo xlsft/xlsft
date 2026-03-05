8e7c90135e5c99ca33a90e54f6b8ced11d3bfdaf
xlsft <dev@xlsft.ru>
2026-03-05 20:05:56 +0300

    feat: useExperience composable

---
+++ app/composables/useExperience.ts
@@ -0,0 +1,18 @@
+export const useExperience = (_from: string | Date, _to?: string | Date) => {
+    const { t, locale } = useI18n()
+    const from = new Date(_from), to = _to ? new Date(_to) : new Date()
+    const total = (to.getFullYear() - from.getFullYear()) * 12 + (to.getMonth() - from.getMonth())
+    const years = Math.floor(total / 12), months = total % 12
+
+    const data = { total, from, to, now: !_to, months, years }
+    return {
+        duration() {  
+            return (data.years > 0 ? useNumberCase(data.years, t('years.one'), t('years.two'), t('years.more')) + ' ' : '') + 
+            useNumberCase(data.months, t('months.one'), t('months.two'), t('months.more')) 
+        },
+        period() {
+            const formatter = new Intl.DateTimeFormat(locale.value, { month: 'long' })
+            return `${formatter.format(data.from)} ${data.from.getFullYear()} — ${data.now ? t('present') : `${formatter.format(data.to)} ${data.to.getFullYear()}`} / ${this.duration()}`
+        }
+    }
+} 
