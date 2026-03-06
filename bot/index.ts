b9ec3fbd70077de409e97cd1e73c2a5e8141fe68
xlsft <dev@xlsft.ru>
2026-03-06 13:09:13 +0300

    feat: basic schema for bot menu

--- bot/index.ts
+++ bot/index.ts
@@ -1,16 +1,120 @@
-import { Bot, type Context } from 'grammy'
+import { Bot, InlineKeyboard, type Context } from 'grammy'
 import { conversations, createConversation, type ConversationFlavor } from "@grammyjs/conversations";
-import { Menu } from "@grammyjs/menu";
+import { useSanity } from './utils/useSanity';
+import { locales, useI18n } from './utils/useI18n';
+import { convert } from 'telegram-markdown-v2';
+import config from '../global.config'
+import { useLocalizedMenu } from './utils/useLocalizedMenu';
+import { defineSection } from './utils/defineSection';
 
-export const bot = new Bot<ConversationFlavor<Context>>(process.env.TG_TOKEN!)
+export const bot = new Bot<ConversationFlavor<Context>>(process.env.TG_TOKEN!); bot.use(conversations())
+export const sanity = useSanity()
 
-bot.use(conversations())
+export const commands = {
+    start: async (ctx: Context) => {
+        const { locale } = useI18n(ctx)
+        const data = await sanity.fetch<{ image: string, content: string, skills: { type: string, items: { name: string, priority: number }[] }[] }>(`*[_type == "summary"]{
+            "image": image.asset->url, 
+            "content": content[$locale],
+            "skills": array::unique(*[_type == "skill"] | order(type asc) { "type": type })[]{
+                "type": type,
+                "items": *[_type == "skill" && type == ^.type] | order(name asc){
+                    name,
+                    priority
+                }
+            },
+        }[0]`, { locale })
 
-const menu = new Menu('index')
-    .text('A', (ctx) => ctx.reply("You pressed A!")).row()
-    .text("B", (ctx) => ctx.reply("You pressed B!"));
-bot.on('message', async (ctx) => {
-    console.log(ctx.from?.language_code)
-})
+        ctx.react('🤝')
+        ctx.replyWithPhoto(data.image, { 
+            caption: convert(`${data.content.replaceAll(/^::.*$/gm, '')}\n${
+                Object.entries(Object.fromEntries(data.skills.map((group) => [
+                    group.type, 
+                    group.items.sort((a, b) => b.priority - a.priority)
+                ])))
+                .sort(([, aItems], [, bItems]) => bItems.length - aItems.length)
+                .map(([label, items]) => `* **${label}**: __${items.sort((a, b) => b.priority - a.priority).map(v => v.name).join(', ')}__`)
+                .join('\n')
+            }`),
+            reply_markup: useLocalizedMenu(ctx, ({ markup, t }) => markup
+                .text(t.sections.experience, `section:experience`)
+                .text(t.sections.education, `section:education`).row()
+                .text(t.sections.projects, `section:projects`)
+                .text(t.labels.contact_me, `section:contact`).row()
+                .url(t.labels.more_about, config.head.url).primary()
+            ), 
+            parse_mode: 'MarkdownV2' 
+        })
+    }
+}
+
+export const sections = {
+    experience: defineSection('experience', async ({ ctx, locale }) => {
+        const data = await sanity.fetch<{ }[]>(`*[_type == "experience"]{
+            id, link,
+            "name": name[$locale],
+            "logo": logo.dark.asset->url,
+            "about": about[$locale],
+            "positions": positions[]{
+                "name": name[$locale],
+                "description": description[$locale],
+                "skills": skills[]->{
+                    name, type, color, priority
+                } | order(priority desc),
+                duration,
+            } | order(duration.from desc),
+            "footer": footer[$locale],
+        } | order(positions[0].duration.from desc)`, { locale })
+        ctx.editMessageCaption({
+            caption: JSON.stringify(data)
+        })
+    })
+}
+
+bot.command('start', commands.start)
+
+
+// bot.callbackQuery('section:education', async (ctx) => {
+//     const { locale } = useI18n(ctx)
+//     const data = await sanity.fetch<{ }[]>(`*[_type == "experience"]{
+//         id, link,
+//         "name": name[$locale],
+//         "logo": logo.dark.asset->url,
+//         "about": about[$locale],
+//         "positions": positions[]{
+//             "name": name[$locale],
+//             "description": description[$locale],
+//             "skills": skills[]->{
+//                 name, type, color, priority
+//             } | order(priority desc),
+//             duration,
+//         } | order(duration.from desc),
+//         "footer": footer[$locale],
+//     } | order(positions[0].duration.from desc)`, { locale })
+
+//     ctx.reply(JSON.stringify(data))
+// })
+
+// bot.callbackQuery('section:projects', async (ctx) => {
+//     const { locale } = useI18n(ctx)
+//     const data = await sanity.fetch<{ }[]>(``, { locale })
+// })
+// const projects = await sanity.fetch<string[]>(`*[_type == "project"].id`); projects.forEach((id) => 
+// bot.callbackQuery(`section:projects:${id}`, async (ctx) => {
+//     const { locale } = useI18n(ctx)
+//     const data = await sanity.fetch<{ }[]>(``, { locale })
+// }))
+
+// bot.callbackQuery('section:contact', async (ctx) => {
+//     const { locale } = useI18n(ctx)
+//     const data = await sanity.fetch<{ }[]>(`{
+//         "links": {
+//             "hh": *[_type == "link" && id == "hh"][0] { "label": label[$locale], to },
+//             "github": *[_type == "link" && id == "github"][0] { "label": label[$locale], to },
+//             "linkedin": *[_type == "link" && id == "linkedin"][0] { "label": label[$locale], to },
+//         },
+//     }`, { locale })
+// })
+
 
 bot.start()
